1b2f7a471a6ccf5a62ff5f72ecda9100b1e6f214
Rustem <mr.kamilyanov@mail.ru>
2023-03-17 11:23:52 +0300

    init commit

---
+++ src/base/settings.py
@@ -0,0 +1,12 @@
+import os
+import sqlite3
+
+PATH_PROJ = '/home/rustem/projects/parkinson'
+PATH_DATA = os.path.join(PATH_PROJ, 'data')
+PATH_DATA_RAW = os.path.join(PATH_DATA, 'raw')
+PATH_DATA_INTERIM = os.path.join(PATH_DATA, 'interim')
+PATH_DATABASE = os.path.join(PATH_DATA, 'db')
+PATH_MODELS = os.path.join(PATH_PROJ, 'models')
+
+connector = sqlite3.connect(
+    os.path.join(PATH_DATABASE, 'mts.db'))
